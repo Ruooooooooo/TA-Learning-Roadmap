f2b23fb18cbb8c62c9b96f4fff5009f33a42ebc8
Ruooooooooo <c69505732@gmail.com>
2026-07-16 14:20:50 +0800

    完成Day1角色状态练习
    # Day 1
    
    ## 今日学习
    
    - int
    
    - float
    
    - string
    
    - bool
    
    - 变量赋值
    
    - Console.WriteLine()
    
    ## 今日收获
    
    变量保存的是当前值。
    
    赋值会覆盖旧值。
    
    代码按顺序执行。
    
    ## 今日困难
    
    无
    
    ## 明日目标
    
    继续训练变量追踪。

---
+++ 01_CSharp_Foundation/Week01_Syntax_VariableTracing/exercises/Day01_PlayerStatus.cs
@@ -0,0 +1,38 @@
+﻿namespace 练习
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string playName = "Artist";
+            int level = 1;
+            int hp = 100;
+            int attack = 18;
+            float moveSpeed = 4.5f;
+            bool isAlive = true;
+            int exp = 80;
+
+            Console.WriteLine("================");
+            Console.WriteLine("Player Status");
+            Console.WriteLine("================");
+            Console.WriteLine("");
+            Console.WriteLine("Name:" + playName );
+            Console.WriteLine("Level:" + level);
+            Console.WriteLine("HP:" + hp);
+            Console.WriteLine("Attack:" + attack);
+            Console.WriteLine("Move Speed:" + moveSpeed);
+            Console.WriteLine("Alive:" + isAlive);
+            Console.WriteLine("Current EXP:" + exp);
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("升级========================================");
+            Console.WriteLine("");
+            Console.WriteLine("Level:" + (level+1));
+            Console.WriteLine("HP:" + (hp+20));
+            Console.WriteLine("Attack:" + (attack+5));
+            Console.WriteLine("Move Speed:" + (moveSpeed+0.5f));
+            Console.WriteLine("Current EXP:" + (exp += 30));
+
+        }
+    }
+}
