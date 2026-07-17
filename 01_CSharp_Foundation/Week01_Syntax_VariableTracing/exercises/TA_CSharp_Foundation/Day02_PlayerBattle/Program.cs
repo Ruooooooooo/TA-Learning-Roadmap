5979aa7d4efe173db6156e3e65f87aff3130eeb5
Ruooooooooo <c69505732@gmail.com>
2026-07-17 22:10:34 +0800

    Complete Day 2 - Arithmetic and Compound Assignment
    这两天属于宝宝难度，复习为主

---
+++ 01_CSharp_Foundation/Week01_Syntax_VariableTracing/exercises/TA_CSharp_Foundation/Day02_PlayerBattle/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Day02_PlayerBattle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string playerName = "Knight";
+            int hp = 100;
+            int attack = 20;
+            int defense = 8;
+            int gold = 50;
+            int exp = 0;
+
+            hp -= 25;
+            Console.WriteLine(hp);
+
+            gold += 80;
+            Console.WriteLine(gold);
+
+            attack += 5;
+            hp += 30;
+            exp += 100;
+
+            Console.WriteLine("攻击力：" + attack);
+            Console.WriteLine("血量：" + hp);
+            Console.WriteLine("经验：" + exp);
+
+            int potion = 3;
+
+            potion--;
+            hp += 40;
+
+            Console.WriteLine("剩余血瓶：" + potion);
+            Console.WriteLine("剩余血量：" + hp);
+        }
+    }
+}
