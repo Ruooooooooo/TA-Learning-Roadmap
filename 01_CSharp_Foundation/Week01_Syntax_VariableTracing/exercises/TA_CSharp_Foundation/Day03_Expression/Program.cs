064c91ad8f745efc113a453848a65318def01960
Ruooooooooo <c69505732@gmail.com>
2026-07-20 23:29:01 +0800

    Day04_MethodScopeLab

---
+++ 01_CSharp_Foundation/Week01_Syntax_VariableTracing/exercises/TA_CSharp_Foundation/Day03_Expression/Program.cs
@@ -0,0 +1,136 @@
+﻿namespace Day03_Expression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            Console.WriteLine("第一题==========");
+            int a = 7;
+            int b = 3;
+
+            int c = a + b * 2;
+            //c = 6+7 = 13
+            a += c / 4;
+            //a = a + c / 4 = a + 3 = 10 
+            b = a % 4 + b;
+            // b = 5
+
+            Console.WriteLine($"{a},{b},{c}");
+            Console.WriteLine("第二题==========");
+
+            int x = 4;
+            int y = x++ + 3;
+            //y = 8
+            int z = ++x + y--;
+            //z = 12
+            x += z % 4;
+            //x = 4
+
+            Console.WriteLine($"x={x},y={y},z={z}");
+            Console.WriteLine("第三题==========");
+
+            int textureCount = 17;
+            int batchSize = 4;
+
+            int fullBatches = textureCount / batchSize;
+            //fullBatches = 17/4 = 4
+            int remainder = textureCount % batchSize;
+            //reaminder = 17 % 4 = 1
+            textureCount -= fullBatches * 2;
+            //textureCount = textureCount - fullBatches * 2
+            //             = 17 - 4 * 2 
+            //             = 9
+
+            Console.WriteLine(
+                $"Textures:{textureCount}, Batches:{batchSize},Reamaining:{remainder}"
+                );
+            Console.WriteLine("第四题==========");
+            int warningCount = 0;
+            int size = 2048;
+
+            bool firstCheck = size > 1024 && warningCount++ == 0;
+            //fales
+            //应该是ture
+            bool secondCheck = size < 4096 || ++warningCount > 10;
+            //ture
+            bool thirdCheck = !firstCheck || warningCount++ > 0;
+            //ture
+
+            Console.WriteLine(
+                $"{firstCheck}|{secondCheck}|{thirdCheck}|{warningCount}"
+            );
+
+            Console.WriteLine("第五题==========");
+            int r = 5;
+            int g = 8;
+            int q = 2;
+
+            int total = (r += 3) + (g -= 2) * q;
+            // 8+6*2 =20
+            q += total % 5;
+            //b=2
+            r = g++ + --q;
+            //r 8+1=9
+
+            Console.WriteLine($"R:{r} G:{g} Q:{q} Total:{total}");
+
+
+
+            #endregion
+
+            Console.WriteLine("");
+            Console.WriteLine("======今日作业==========");
+            Console.WriteLine("");
+
+            int 玩家攻击力 = 37;
+            int 武器加成 = 11;
+            int 连击倍率 = 3;
+            int 敌人防御力 = 14;
+            int 护甲穿透 = 5;
+            int 玩家当前体力 = 23;
+            int 本次攻击体力消耗 = 7;
+            int 敌人当前生命值 = 96;
+
+            int 基础攻击力 = 玩家攻击力 + 武器加成;
+            int 连击伤害 = 基础攻击力 *= 连击倍率;
+            int 实际总伤害 = (基础攻击力 + 连击伤害) - (敌人防御力 - 护甲穿透);
+            int 敌人剩余血量 = 敌人当前生命值 - 实际总伤害;
+            int 玩家剩余血量 = 玩家当前体力 - 本次攻击体力消耗;
+            bool 获胜 = 敌人剩余血量 <= 0 && 玩家剩余血量 >= 0;
+            bool 是否能再次攻击 = 玩家剩余血量 > 本次攻击体力消耗;
+
+
+            Console.WriteLine(
+                $"【原始数据】玩家攻击力：{玩家攻击力}，武器加成：{武器加成}，连击倍率：{连击倍率}"
+            );
+
+            Console.WriteLine(
+                $"【原始数据】敌人生命值：{敌人当前生命值}，敌人防御力：{敌人防御力}，护甲穿透：{护甲穿透}"
+            );
+
+            Console.WriteLine(
+                $"【原始数据】玩家当前体力：{玩家当前体力}，本次攻击体力消耗：{本次攻击体力消耗}"
+            );
+
+            Console.WriteLine($"【中间结果】基础攻击力：{基础攻击力}");
+
+            Console.WriteLine(
+                $"【中间结果】连击伤害：{连击伤害}"
+            );
+
+            Console.WriteLine(
+                $"【中间结果】实际总伤害：{实际总伤害}，敌人剩余血量：{敌人剩余血量}"
+            );
+
+            Console.WriteLine($"【中间结果】玩家剩余体力：{玩家剩余血量}");
+
+            Console.WriteLine($"【最终状态】敌人是否被击败：{获胜}");
+
+            Console.WriteLine($"【最终状态】玩家是否能再次攻击：{是否能再次攻击}");
+
+
+
+        }
+    }
+}
---
+++ 01_CSharp_Foundation/Week01_Syntax_VariableTracing/exercises/TA_CSharp_Foundation/Day04_MethodScopeLab/Program.cs
@@ -0,0 +1,144 @@
+﻿namespace Day04_MethodScopeLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            //static int AddCharge(int energy, int bonus)
+            //{
+            //    energy += bonus;
+            //    return energy;
+            //}
+
+            //int energy = 20;
+            //int nextEnergy = AddCharge(energy, 5);
+
+            //Console.WriteLine($"{energy},{nextEnergy}");
+
+            //static int Evaluate(int value)
+            //{
+            //    value *= 2;
+            //    value += 3;
+            //    return value;
+            //}
+
+            //int a = 4;
+            //int b = Evaluate(a + 1);
+            //a += b % 4;
+
+            //Console.WriteLine($"{a},{b}");
+
+            //static int Trim(int value)
+            //{
+            //    value -= 2;
+            //    return value;
+            //}
+
+            //static int Score(int x, int y)
+            //{
+            //    int result = Trim(x) + Trim(y);
+            //    return result;
+            //}
+
+            //int x = 8;
+            //int y = 5;
+            //int result = Score(Trim(x), y);
+
+            //Console.WriteLine($"{x},{y},{result}");
+
+
+            //static int ConvertSize(int size)
+            //{
+            //    int level = size / 4;
+            //    level = 19 / 4 = 5
+            //    return level + size % 4;
+            //    5 + 3 = 8
+            //}
+
+            //int size = 19;
+            //int level = ConvertSize(size);
+            //level = 8
+
+            //{
+            //    int extra = ConvertSize(level);
+            //    Console.WriteLine($"{size},{level},{extra}");
+            //}
+
+            //Console.WriteLine(level);
+            #endregion
+
+            Console.WriteLine("今日作业========================");
+
+            RunExerciseA();
+
+        }
+
+            static int CalculateEnergyCost(int baseCost, int extraCost)
+            {
+                int finallyCost = baseCost + extraCost;
+
+                return finallyCost;
+            }
+
+        static bool CanUseSkill(int currentEnergy, int energyCost)
+        {
+            if (currentEnergy < energyCost)
+            {
+                return false;
+            }
+
+            else 
+            {
+                return true; 
+            }
+        }
+
+            static void RunExerciseA()
+            {
+                string PlayerName = "若见花";
+                int Playerattack = 35;
+                int Playerenergy = 70;
+                int erenergy = Playerenergy;
+
+                string monsterName = "石头人";
+                int Monsterlife = 120;
+                int Monsterdefanse = 18;
+
+                string SkillName1 = "Arc Slash";
+                int SkillPower1 = 30;
+                int EnergyConsumption1 = 20;
+                bool criticalHit1 = false;
+                int skill1ExtraCost1 = 0;
+                int skill1FinalCost = CalculateEnergyCost(EnergyConsumption1, skill1ExtraCost1);
+
+                string SkillName2 = "Burst Edge";
+                int SkillPower2 = 45;
+                int EnergyConsumption2 = 25;
+                bool criticalHit2 = true;
+                int skillExtraCost2 = 10;
+                int skill2FinalCost = CalculateEnergyCost(EnergyConsumption2, skillExtraCost2);
+
+                Console.WriteLine("玩家：" + PlayerName);
+                Console.WriteLine("攻击力：" + Playerattack);
+                Console.WriteLine("当前能量：" + Playerenergy);
+                Console.WriteLine("敌人：" + monsterName);
+                Console.WriteLine("防御力：" + Monsterdefanse);
+                Console.WriteLine("当前生命：" + Monsterlife);
+
+                bool canUseSkill = CanUseSkill(Playerenergy, skill1FinalCost);
+                Console.WriteLine("第一技能是否能释放：" + canUseSkill);
+
+                if (canUseSkill == true) 
+                {
+                    erenergy = erenergy - skill1FinalCost;
+                    Console.WriteLine("技能释放成功");
+                }
+                else 
+                {
+                Console.WriteLine("技能释放失败");
+                }
+
+            }
+        }
+    }
